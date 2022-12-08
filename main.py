cf289cdb481abafcd163eda83e87a55e51f0e610
Vicente Sena <vicentecsena@gmail.com>
2022-12-08 21:18:34 +0000

    Change game title

--- main.py
+++ main.py
@@ -103,7 +103,7 @@ player_stand = pygame.image.load('graphics/player/player_stand.png').convert_alp
 player_stand = pygame.transform.rotozoom(player_stand,0,2)
 player_stand_rect = player_stand.get_rect(center = (400,200))
 
-game_name = test_font.render('Pixel Runner',False,(111,196,169))
+game_name = test_font.render('Jump\'N\'Run',False,(111,196,169))
 game_name_rect = game_name.get_rect(center = (400,80))
 
 game_message = test_font.render('Carregue na tecla de Espaço para iniciar',False,(111,196,169))
