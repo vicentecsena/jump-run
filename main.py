38d867deae2d73c3d325b49ea80db17b7c82ae19
Vicente Sena <vicentecsena@gmail.com>
2022-12-09 00:09:04 +0000

    change size of player in starting screen

--- main.py
+++ main.py
@@ -90,7 +90,7 @@ player_gravity = 0
 
 # Ecrã inicial
 player_stand = pygame.image.load('graphics/player/jump.png').convert_alpha()
-player_stand = pygame.transform.scale(player_stand , (64, 92))
+player_stand = pygame.transform.scale(player_stand , (128, 184))
 player_stand_rect = player_stand.get_rect(center = (400,200))
 
 game_name = test_font.render('Jump\'N\'Run',False,(111,196,169))
